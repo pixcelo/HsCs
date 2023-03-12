9bfd56f4910d70462ada6e4a07b6cf5c33ac1b45
pixcelo <bodomu@gmail.com>
2023-03-12 16:24:06 +0900

    websocket接続

---
+++ HsCs/HsCs/BitFlyerWebSocketClient.cs
@@ -0,0 +1,97 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace HsCs
+{
+    public class BitFlyerWebSocketClient
+    {
+        private readonly Uri _uri;
+        private ClientWebSocket _webSocket;
+
+        public BitFlyerWebSocketClient(Uri uri)
+        {
+            _uri = uri;
+        }
+
+        public async Task StartAsync(Action<BitFlyerExecution> onExecution)
+        {
+            _webSocket = new ClientWebSocket();
+            await _webSocket.ConnectAsync(_uri, CancellationToken.None);
+
+            var jsonRequest = Encoding.UTF8.GetBytes(@"{""jsonrpc"":""2.0"",""method"":""subscribe"",""params"":{""channel"":""lightning_executions_FX_BTC_JPY""},""id"":""1""}");
+            await _webSocket.SendAsync(new ArraySegment<byte>(jsonRequest), WebSocketMessageType.Text, true, CancellationToken.None);
+
+            var buffer = new byte[1024 * 4];
+            var jsonOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            while (_webSocket.State == WebSocketState.Open)
+            {
+                var result = await _webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                var jsonString = Encoding.UTF8.GetString(buffer, 0, result.Count);
+
+                // Ignore any message that is not a JSON - RPC notification
+                if (!jsonString.StartsWith(@"{""jsonrpc"":""2.0"",""method"":""", StringComparison.Ordinal))
+                {
+                    continue;
+                }
+
+                var jsonDocument = JsonDocument.Parse(jsonString);                
+                var respnse = JsonSerializer.Deserialize<BitFlyerResponse>(jsonDocument.RootElement.GetProperty("params").GetRawText(), jsonOptions);
+
+                foreach (var execution in respnse.Message)
+                {
+                    onExecution?.Invoke(execution);
+                }
+            }
+
+            await _webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+        }
+
+        public async Task StopAsync()
+        {
+            if (_webSocket.State == WebSocketState.Open)
+            {
+                await _webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+            }
+        }
+    }    
+
+    public class BitFlyerResponse
+    {
+        [JsonPropertyName("channel")]
+        public string Channel { get; set; }
+
+        [JsonPropertyName("message")]
+        public BitFlyerExecution[] Message { get; set; }
+    }
+
+    public class BitFlyerExecution
+    {
+        [JsonPropertyName("id")]
+        public long Id { get; set; }
+
+        [JsonPropertyName("side")]
+        public string Side { get; set; }
+
+        [JsonPropertyName("price")]
+        public double Price { get; set; }
+
+        [JsonPropertyName("size")]
+        public double Size { get; set; }
+
+        [JsonPropertyName("exec_date")]
+        public DateTime ExecDate { get; set; }
+
+        [JsonPropertyName("buy_child_order_acceptance_id")]
+        public string BuyChildOrderAcceptanceId { get; set; }
+
+        [JsonPropertyName("sell_child_order_acceptance_id")]
+        public string SellChildOrderAcceptanceId { get; set; }
+    }
+    
+}
--- HsCs/HsCs/Program.cs
+++ HsCs/HsCs/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Net.WebSockets;
-using System.Text.Json;
+﻿using HsCs;
 
 namespace hscs
 {
@@ -7,39 +6,13 @@ namespace hscs
     {
         static async Task Main(string[] args)
         {
-            //var url = new Uri("wss://stream.bybit.com/realtime_public");
-            var url = new Uri("wss://stream-testnet.bybit.com/v5/public/linear");
-            using (var client = new ClientWebSocket())
+            var client = new BitFlyerWebSocketClient(new Uri("wss://ws.lightstream.bitflyer.com/json-rpc"));
+            await client.StartAsync((execution) =>
             {
-                await client.ConnectAsync(url, CancellationToken.None);
-                Console.WriteLine("WebSocket connected");
-
-                //var channel = "instrument_info.100ms.BTCUSDT";
-                var channel = "kline.1.BTCUSDT";
-                var request = new { op = "subscribe", args = new string[] { channel } };
-                var json = JsonSerializer.Serialize(request);
-
-                var bytes = System.Text.Encoding.UTF8.GetBytes(json);
-                var buffer = new ArraySegment<byte>(bytes);
-
-                await client.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
-
-                var responseBuffer = new byte[1024 * 4];
-                while (client.State == WebSocketState.Open)
-                {
-                    var result = await client.ReceiveAsync(new ArraySegment<byte>(responseBuffer), CancellationToken.None);
-                    if (result.MessageType == WebSocketMessageType.Close)
-                    {
-                        await client.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
-                        Console.WriteLine("WebSocket closed");
-                    }
-                    else
-                    {
-                        var message = System.Text.Encoding.UTF8.GetString(responseBuffer, 0, result.Count);
-                        Console.WriteLine($"Received message: {message}");
-                    }
-                }
-            }
+                Console.WriteLine($"Executed {execution.Side} {execution.Size} BTC at {execution.Price} JPY ({execution.ExecDate})");
+            });
         }
     }
+
+
 }
