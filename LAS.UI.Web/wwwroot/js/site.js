3115767ce81fe589576e142db4acd17c4c76b33a
pixcelo <bodomu@gmail.com>
2024-06-29 05:17:02 +0900

    mod: API呼び出しテスト オブジェクトを取得してコンソール出力

--- LAS.UI.Web/wwwroot/js/site.js
+++ LAS.UI.Web/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// API呼び出しテスト
+const getSample = document.getElementById("get-sample");
+getSample.addEventListener("click", async () => {
+    console.log("testGet");
+
+    // API呼び出し
+    const response = await fetch("https://localhost:7037/api/sample/get");
+    const data = await response.json();
+    console.log(data);
+
+});
+
+const postSample = document.getElementById("post-sample");
+postSample.addEventListener("click", async () => {
+    console.log("testPost");
+});
