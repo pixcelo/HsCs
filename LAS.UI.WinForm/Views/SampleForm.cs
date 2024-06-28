61f0f9ae6fc33132dbf7c180319cb9a71e8598ca
pixcelo <bodomu@gmail.com>
2024-06-29 05:58:47 +0900

    mod: WinFormからAPI呼び出しテスト

--- LAS.UI.WinForm/Views/SampleForm.cs
+++ LAS.UI.WinForm/Views/SampleForm.cs
@@ -1,10 +1,16 @@
+using LAS.Lib.WebAccessor;
+
 namespace LAS.UI.WinForm.Views
 {
     public partial class SampleForm : Form
     {
+        private readonly ApiClient apiClient;
+
         public SampleForm()
         {
             InitializeComponent();
+
+            this.apiClient = new ApiClient();
         }
 
         /// <summary>
@@ -12,10 +18,20 @@ namespace LAS.UI.WinForm.Views
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void getbutton_Click(object sender, EventArgs e)
+        private async void getbutton_Click(object sender, EventArgs e)
         {
-            // 別ライブラリのクラスを呼び出す
+            // API呼び出しのテスト
+            string endpoint = "https://localhost:7037/api/sample/get";
+            string result = await apiClient.GetAsync(endpoint);
 
+            if (result != null)
+            {
+                MessageBox.Show(result, "API Response", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Failed to get response from API.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
