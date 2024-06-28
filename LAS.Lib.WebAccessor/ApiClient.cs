9942480336317086cba2f23cb467fbe213ca28a0
pixcelo <bodomu@gmail.com>
2024-06-29 05:58:07 +0900

    add: APIクライアント

---
+++ LAS.Lib.WebAccessor/ApiClient.cs
@@ -0,0 +1,29 @@
+﻿namespace LAS.Lib.WebAccessor
+{
+    public class ApiClient
+    {
+        private readonly HttpClient httpClient;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public ApiClient()
+        {
+            this.httpClient = new HttpClient();
+        }
+
+        public async Task<string> GetAsync(string url)
+        {
+            try
+            {
+                var response = await this.httpClient.GetAsync(url);
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Request error: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
