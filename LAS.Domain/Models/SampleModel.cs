4fc85496acd7cfe87cd4eb6a27bf82bf9d814166
pixcelo <bodomu@gmail.com>
2024-06-29 07:17:00 +0900

    add: Domainプロジェクト追加

---
+++ LAS.Domain/Models/SampleModel.cs
@@ -0,0 +1,20 @@
+﻿namespace LAS.Domain.Models
+{
+    public class SampleModel
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// 値
+        /// </summary>
+        public string? Value { get; set; }
+    }
+}
